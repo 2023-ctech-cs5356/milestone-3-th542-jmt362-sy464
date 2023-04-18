a54bf9c31e31a8dd593d7466f04cfdd108cbfa46
Jonathan Tan <jmtan188@gmail.com>
2023-04-18 15:44:20 +0000

    Add readme, install bulma

--- ctswap/settings.py
+++ ctswap/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'bulma',
 ]
 
 MIDDLEWARE = [
